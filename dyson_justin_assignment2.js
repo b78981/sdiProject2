49810518850b98003d0d4a0d69c4c22d817796ca
Justin Dyson <jdyson@fullsail.edu>
2014-01-14 22:04:05 -0600

    added boolean function

--- dyson_justin_assignment2.js
+++ dyson_justin_assignment2.js
@@ -7,12 +7,13 @@ Completed 01/16/14
 
 
 // Global Variables
-var userName;
-var userPokeCount;
+var userName; // string
+var userPokeCount; // number
+var isUserReady; // boolean
 
 // Functions
 
-var findPokeTask = function (pokeCount) {
+var findPokeTask = function (pokeCount) {		//procedure: based on pokeCount, announces the next step for Pokemon trainer
 	var pokeCount;
 	
 	if (pokeCount < 6) {
@@ -22,6 +23,23 @@ var findPokeTask = function (pokeCount) {
 		console.log("You need to start training your Pokemon!");
 		};
 	return;
+	};
+	
+var catchPoke = function (userIsReady,pokeCount) { 			//boolean function: returns beltIsFull
+	if (userIsReady) {
+		while (pokeCount < 6) {
+			pokeCount ++;
+			console.log("Great " + userName + ", you now have " + pokeCount + " pokemon on your Poke-belt!");
+			};
+	}
+	else {
+		console.log("Too bad! The wild Pokemon aren't waiting for you to be ready; here they come!");
+		while (pokeCount < 6) {
+			pokeCount ++;
+			console.log("Great " + userName + ", you now have " + pokeCount + " pokemon on your Poke-belt!");
+			}
+		};
+		return beltIsFull, pokeCount;
 	};
 
 
@@ -33,8 +51,11 @@ userPokeCount = parseInt(prompt(userName + ", how many Pokemon do you have?", "E
 
 findPokeTask(userPokeCount); 
 
-confirm("Alright, " + userName + " are you ready to start exploring?");
+isUserReady = confirm("Alright, " + userName + " are you ready to start exploring?");
 
+catchPoke(isUserReady, userPokeCount);
+
+console.log(catchPoke(pokeCount));
 
 
 
