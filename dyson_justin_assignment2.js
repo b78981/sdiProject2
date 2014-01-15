735107b6a6b59e73ce916d132810635a138a6c0d
Justin Dyson <jdyson@fullsail.edu>
2014-01-14 21:02:07 -0600

    first procedure

--- dyson_justin_assignment2.js
+++ dyson_justin_assignment2.js
@@ -1,1 +1,27 @@
-alert("JavaScript works!");
+/*
+By Justin Dyson
+SDI Assignment 2
+Term 1401
+Completed 01/16/14
+*/
+
+
+// Global Variables
+var userPokeCount;
+
+// Functions
+
+var getPokeCount = function (pokeCount) {
+	if (pokeCount < 6) {
+		console.log("We need to catch some Pokemon!");
+		} else {
+			console.log("We don't have anymore room for new Pokemon...");
+			};
+	};
+
+
+// Main Code
+
+
+userPokeCount = prompt("How many Pokemon do you have?", "Enter value 1-6");
+getPokeCount(userPokeCount);
