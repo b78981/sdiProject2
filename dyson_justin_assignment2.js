e6670547ae89167720585f8d7376e8cb81551e5f
Justin Dyson <jdyson@fullsail.edu>
2014-01-15 20:47:05 -0600

    comments added for clarity

--- dyson_justin_assignment2.js
+++ dyson_justin_assignment2.js
@@ -20,7 +20,7 @@ var pokeAndExpTotals; // string value retuned from getPokeAndExpTotalsString
 
 // Functions
 
-//procedure: based on userPokeCount, announces the next step for Pokemon trainer
+//procedure: suggests the next step for Pokemon trainer based on the number of Pokemon he/she has
 var suggestPokeTask = function (pokeCount) {			
 	if (pokeCount < 6) {
 		console.log("You need to catch some Pokemon!");
@@ -33,7 +33,7 @@ var suggestPokeTask = function (pokeCount) {
 	
 };
 
-//boolean function: returns boolean value of TRUE	
+//boolean function: simulates catching Pokemon, returns boolean value of TRUE	
 var catchPoke = function (addToBelt,pokeCount) {
 	while (addToBelt) {
 		if (pokeCount < 6){
@@ -49,7 +49,7 @@ var catchPoke = function (addToBelt,pokeCount) {
 	return true;
 };
 
-// number function: returns a number called totalExp
+// number function: simulates training Pokemon to gain experience Returns a number called totalExp.
 var trainPoke = function (trainingRounds) {
 	var rattataExp = 22;	// number of experience points earned 
 	var pidgeyExp = 17;	// number of experience points earned 
@@ -72,7 +72,7 @@ var trainPoke = function (trainingRounds) {
 	
 };
 	
-// string function, returns string called 
+// string function: concatenates 2 strings that announce the total amount of Pokemon and Exp gained, returns string called totalCountsString
 var getPokeAndExpTotalsString = function (pokeCountString, expCountString) {
 	var totalCountsString;
 	
@@ -87,26 +87,28 @@ userName = prompt("Hi! What's your name?","");
 
 userPokeCount = parseInt(prompt(userName + ", how many Pokemon do you have?", "Enter value 1-6"));
 
-suggestPokeTask(userPokeCount); 
+suggestPokeTask(userPokeCount); // userPokeCount passes the number of Pokemon the user has to the function
 
 wantsPokemon = confirm("Alright, " + userName + " do you want to catch more Pokemon?");
 
 if (wantsPokemon){
-	fullBelt = catchPoke(wantsPokemon, userPokeCount); 
+	fullBelt = catchPoke(wantsPokemon, userPokeCount); // wantsPokemon passes boolean= true & userPokeCount is number of Pokemon the user has
 	userPokeCount = 6;
 	trainingBattles = prompt("Gain Exp by training. How many training battles do you want to have?","Choose 1-3");
-	totalExpGained = trainPoke(trainingBattles);
+	totalExpGained = trainPoke(trainingBattles); // trainingBattles is number passed so loop can run 1-3 times
 }
 else {
 	console.log("Alright, let's start training.");
 	trainingBattles = prompt("Gain Exp by training. How many training battles do you want to have?","Choose 1-3");
-	totalExpGained = trainPoke(trainingBattles);
+	totalExpGained = trainPoke(trainingBattles); // trainingBattles is number passed so loop can run 1-3 times
 };
 	
 finalPokeCountString = "you have " + userPokeCount + " Pokemon"
 finalExpCountString = "you gained " + totalExpGained + " experience"
 
 pokeAndExpTotals = getPokeAndExpTotalsString(finalPokeCountString, finalExpCountString);
+/* finalPokeCountString is a string passed to provide userPokeCount total & finalExpCountString is a string passed to 
+provide totalExpGained total*/
 
 console.log(pokeAndExpTotals);
 		
